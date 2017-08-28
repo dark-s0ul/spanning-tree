0d005f231f6f77d3da604d85d96e177bbd667547
Max Pasichnyk <maximmkaaa@gmail.com>
2017-08-29 01:52:57 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,21 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
-namespace WindowsFormsApplication4
-{
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
-    }
+namespace WindowsFormsApplication4 {
+	static class Program {
+		[STAThread]
+		static void Main() {
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
 }
